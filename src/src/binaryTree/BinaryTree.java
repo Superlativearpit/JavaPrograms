661ef204c6b0b98fafbeb771c17c53e4804905de
Superlativearpit <nameisarpittripathi@gmail.com>
2021-10-10 23:57:58 +0530

    Pre & post order
    Pre & post order

--- src/src/binaryTree/BinaryTree.java
+++ src/src/binaryTree/BinaryTree.java
@@ -2,6 +2,11 @@ package src.binaryTree;
 
 public class BinaryTree {
 
+    //Algorithm Inorder(tree)
+   //1. Traverse the left subtree, i.e., call Inorder(left-subtree)
+   //2. Visit the root.
+   //3. Traverse the right subtree, i.e., call Inorder(right-subtree)
+    //8, 15, 3, 20, 10, 16
     public static void inOrderTraversal(Node root) {
         if (root == null) {
             return;
@@ -17,6 +22,34 @@ public class BinaryTree {
             inOrderTraversal(root.right);
         }
     }
+    
+    public static void preOrderTraversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + ", ");
+
+        if(root.left != null) {
+            preOrderTraversal(root.left);
+        }
+
+        if(root.right != null) {
+            preOrderTraversal(root.right);
+        }
+
+    }
+
+    public static void postOrderTraversal(Node root) {
+        if (root == null)
+            return;
+
+        // Traverse left
+        postOrderTraversal(root.left);
+        // Traverse right
+        postOrderTraversal(root.right);
+        // Traverse root
+        System.out.print(root.data + ", ");
+    }
 
     public static void main(String[] args) {
         Node root = new Node(20, null, null);
@@ -26,8 +59,14 @@ public class BinaryTree {
         root.left.right = new Node(3, null, null);
         root.right.right = new Node(16, null, null);
 
+        System.out.print("In Order Traversal : ");
         BinaryTree.inOrderTraversal(root);
-
+        System.out.println();
+        System.out.print("Pre Order Traversal : ");
+        BinaryTree.preOrderTraversal(root);
+        System.out.println();
+        System.out.print("Post Order Traversal : ");
+        BinaryTree.postOrderTraversal(root);
     }
 
 }
--- src/src/sorting/Main.java
+++ src/src/sorting/Main.java
@@ -2,7 +2,9 @@ package src.sorting;
 
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {7,3,5,2,3,1,5,8};
+        int arr[] = {5,7,6,1,3,2,4};
+        //QuickSort qsort = new QuickSort();
+        //qsort.take(arr);
         MergeSort msort = new MergeSort();
         msort.sort(arr);
         for(int i = 0; i < arr.length; i++) {
