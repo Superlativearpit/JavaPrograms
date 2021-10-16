b5364c11b996c8ca2257429216a6b3f82e868efd
Superlativearpit <nameisarpittripathi@gmail.com>
2021-10-16 10:20:30 +0530

    Changes

--- src/Task1.java
+++ src/Task1.java
@@ -1,8 +1,13 @@
 public class Task1 {
     public static void main(String[] args) {
+        int array[] = new int[5];
+        int a = 12;
+        for (int i = 0; i < 5; i++, a++) {
+            array[i] = a;
+        }
         //Circular Series :-
-        for(int i = 0; i <= 20; i++) {
-            System.out.print(i % 6 + ", ");
+        for(int i = 0; i < 20; i++) {
+            System.out.print(array[i % 5] + ", ");
         }
     }
 }
