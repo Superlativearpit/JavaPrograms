3914ac63161377a09a21a884200e9dc6c06dbc8f
Superlativearpit <nameisarpittripathi@gmail.com>
2021-12-16 01:30:06 +0530

    Command Line

---
+++ src/src/jviva/CommandLineArguement.java
@@ -0,0 +1,8 @@
+package src.jviva;
+
+public class CommandLineArguement{
+    public static void main(String[] args) {
+        String a = "University";
+        System.out.println(args[0] + " "+ a + " " + args[1]);
+    }
+}
