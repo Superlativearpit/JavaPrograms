a41b5527e5ed9e868ec8a36bc3a4437d2a4b1bf5
Arpit Tripathi <nameisarpittripathi@gmail.com>
2022-03-08 12:37:44 +0530

    Algo by Robert Sedgwick

--- src/src/sorting/SelectionSort.java
+++ src/src/sorting/SelectionSort.java
@@ -19,4 +19,25 @@ public class SelectionSort {
         }
     }
 
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        for(int i = 0; i <= n; i++) {
+            int min = i;
+            for(int j = i + 1; j < n; j++) {
+                if(less(a[i], a[min]))
+                    min = j;
+                exch(a, i, min);
+            }
+        }
+    }
+
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
 }
